b6f358b4751dc112de81314bc5b1a8f473225653
Rodrigo Augusto <90705969+rodrigAugust@users.noreply.github.com>
2022-10-14 22:21:05 -0300

    commit inicial

---
+++ src/js/index.js
@@ -0,0 +1,28 @@
+const tabs = console.log(document.querySelectorAll(".tab"));
+
+tabs.forEach(tab => {
+    tab.addEventListener("click", function () {
+        if (tab.classList.contains("select")) {
+            return;
+        }
+
+        selectTab(tab);
+        selectInfo(tab);
+    });
+});
+
+function selectTab(tab){
+    const tabSelect = document.querySelector(".tab.select");
+    tabSelect.classList.remove("select");
+    tab.classList.add("select");
+}
+
+function selectInfo(tab){
+    const infoSelect = document.querySelector("info.select");
+    infoSelect.classList.remove("select");
+
+    const idDoElementosDeInformacoes = `info-${tab.id}`
+
+    const infoASerMostrada = document.getElementsById(idDoElementosDeInformacoes);
+    infoASerMostrada.classList.add("select");
+}
